a1904fd4207c41bc6d103793fb7c3f9b3d312e6d
Ana Jackeline <annajacke2@gmail.com>
2025-05-06 23:25:51 -0300

    Add files via upload

---
+++ Questao6/main.c
@@ -0,0 +1,41 @@
+/*6. Soma de Matrizes
+Crie duas matrizes 3x3, some seus valores e exiba o resultado.*/
+
+#include <stdio.h>
+
+int main() {
+    int A[3][3], B[3][3], C[3][3];
+    int i, j;
+
+    printf("Digite os elementos da matriz A (3x3):\n");
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            scanf("%d", &A[i][j]);
+        }
+    }
+
+    printf("Digite os elementos da matriz B (3x3):\n");
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            scanf("%d", &B[i][j]);
+        }
+    }
+
+    // Soma das matrizes
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            C[i][j] = A[i][j] + B[i][j];
+        }
+    }
+
+    // Impressão da matriz resultante
+    printf("Matriz resultante da soma A + B:\n");
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            printf("%d ", C[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
